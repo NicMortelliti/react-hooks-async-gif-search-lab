91089e1e1db543ad46da86c6a89e8af6c5409325
NicMortelliti <npmortelliti@gmail.com>
2022-05-11 14:57:16 -0700

    Add fetch request to GifListContainer

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,8 @@ import React from "react";
 import NavBar from "./NavBar";
 import GifListContainer from "./GifListContainer";
 
-const API = "https://api.giphy.com/v1/gifs/search?q=";
-const KEY = "CBCoUOxg2ZFLDRCRYI6rKBdnumF21LcM";
-
 function App() {
-  // const fetchGifs = () => {
-  //   fetch(`${API}${query}&api_key=${KEY}&rating=g`);
-  // };
+
 
   return (
     <div>
--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -1,6 +1,17 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+const API = "https://api.giphy.com/v1/gifs/search?q=";
+const KEY = "CBCoUOxg2ZFLDRCRYI6rKBdnumF21LcM";
 
 function GifListContainer() {
+  const query = "dolphin";
+
+  useEffect(() => {
+    fetch(`${API}${query}&api_key=${KEY}&rating=g`)
+      .then((r) => r.json())
+      .then((data) => console.log(data));
+  }, []);
+
   return <p>GifListContainer</p>;
 }
 
