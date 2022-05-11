66e9adbde2132bdb1434546095b07ba119bd4cf6
NicMortelliti <npmortelliti@gmail.com>
2022-05-11 15:42:16 -0700

    Only grab the first 3 giphy images from the fetch results.

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -4,12 +4,13 @@ const API = "https://api.giphy.com/v1/gifs/search?q=";
 const KEY = "CBCoUOxg2ZFLDRCRYI6rKBdnumF21LcM";
 
 function GifListContainer() {
+  const [gifArray, setGifArray] = useState([]);
   const query = "dolphin";
 
   useEffect(() => {
     fetch(`${API}${query}&api_key=${KEY}&rating=g`)
       .then((r) => r.json())
-      .then((data) => console.log(data));
+      .then((data) => setGifArray(data.data.slice(0, 3)));
   }, []);
 
   return <p>GifListContainer</p>;
